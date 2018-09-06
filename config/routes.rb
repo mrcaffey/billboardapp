2565ce08367f2363acf7cac0daf0430d0491310f
Nate <nathanestes@gmail.com>
2018-09-06 13:53:17 -0600

    i dont know

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,8 @@
 Rails.application.routes.draw do
 
- 
- #get 'artists/name'
-  #get 'artists/genre'
-  #get 'charts/rank'
-  #get 'charts/name'
-  #get 'charts/edit'
    root 'charts#index'
-  
-
+   
   resources :charts, :artist
+  resources: artists 
 
-
-  #get 'chart/index'
-  #get 'chart/show'
-  #get 'chart/new'
-  #get 'artist/index'
-  #get 'artist/show'
-  #get 'artist/new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

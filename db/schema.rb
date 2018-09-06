bd6873be4f738058983c60e3269136544277c8d8
Micah Caffey <mrcaffey@gmail.com>
2018-09-05 19:34:16 -0600

    added song model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_05_220154) do
+ActiveRecord::Schema.define(version: 2018_09_06_013345) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,12 @@ ActiveRecord::Schema.define(version: 2018_09_05_220154) do
     t.integer "rank"
     t.string "song"
     t.string "artist"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

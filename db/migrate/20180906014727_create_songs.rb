34cd3f3af23cfaf4242de75e79436c5518b5c252
Micah Caffey <mrcaffey@gmail.com>
2018-09-06 14:06:37 -0600

    misc. changes

--- db/migrate/20180906014727_create_songs.rb
+++ db/migrate/20180906014727_create_songs.rb
@@ -1,7 +1,9 @@
 class CreateSongs < ActiveRecord::Migration[5.2]
   def change
     create_table :songs do |t|
-      t.string :title
+      t.string :index
+      t.string :new
+      t.string :show
 
       t.timestamps
     end

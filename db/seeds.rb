95769bcd37d9746151003875dcc491d7aa7c2fbb
Micah Caffey <mrcaffey@gmail.com>
2018-09-06 22:02:41 -0600

    updated schema file with remaining artists

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,24 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-#roles = ['teacher', 'ta', 'student']
 
-#10.times do |i|
- #course = Course.create(name: "course_#{i}")
-
- #10.times do |j|
- #  user = User.create(
-  #   first_name: "first_#{i}_#{j}",
-  #   last_name: "last_#{i}_#{j}"
-  # )
-
-  # Enrollment.create(
-  #   course_id: course.id,
-  #   user_id: user.id,
-  #   role: roles.sample
-  # )
- #end
-#end
 
 Artist.create(name: "Drake", genre: "Hip-Hop")
 Artist.create(name: "Ariana Grande", genre: "Pop")
